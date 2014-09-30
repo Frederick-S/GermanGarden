29b087e29324cab91c18ef4beb2a9b40440a8d88
Frederick-S <mao_xiaodan@hotmail.com>
2014-09-30 08:05:56 +0800

    Add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        cssmin: {
+            css: {
+                src: 'css/style.css',
+                dest: 'css/style.min.css'
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'js/gm.min.js': ['js/gm.js']
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['cssmin:css', 'uglify:js']);
+};
